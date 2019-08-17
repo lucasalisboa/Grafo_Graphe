cfa9d24375fee245bb7fda21a642801f03f25b6e
lucasalisboa <lucasa.lisboa@hotmail.com>
2019-08-16 22:41:49 -0300

    Enum Color

---
+++ src/Grafo/Color.java
@@ -0,0 +1,7 @@
+package Grafo;
+
+public enum Color {
+    WHITE,
+    GRAY,
+    BLACK;
+}
--- src/Grafo/No.java
+++ src/Grafo/No.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class No {
     public List<Incidencia> incidencias;
+    public Color estado;
 
     public No() {
         incidencias = new ArrayList<>();
